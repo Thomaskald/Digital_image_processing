68d4761abb9d4c0fc5f28b2f2d036e2b7dba1f27
thomas <thomaskald849@gmail.com>
2025-05-01 21:22:18 +0300

    A, B from q4 done

---
+++ q4.py
@@ -0,0 +1,38 @@
+from matplotlib import pyplot as plt
+from skimage import io, filters
+from skimage.filters import median
+from skimage.morphology import disk
+
+image1 = io.imread('/home/thomas/Digital_Image_Processing/DIP-project-1/DIP-project-1/images-project-1/lenna-n1.jpg', as_gray=True)
+image2 = io.imread('/home/thomas/Digital_Image_Processing/DIP-project-1/DIP-project-1/images-project-1/lenna-n2.jpg', as_gray=True)
+image3 = io.imread('/home/thomas/Digital_Image_Processing/DIP-project-1/DIP-project-1/images-project-1/lenna-n3.jpg', as_gray=True)
+
+#Εικόνα 1
+img1_filtered = filters.gaussian(image1, 3)
+
+plt.figure(figsize=(10,5))
+plt.subplot(1, 2, 1)
+plt.imshow(image1, cmap='gray')
+plt.title('Noisy 1')
+plt.axis('off')
+plt.subplot(1, 2, 2)
+plt.imshow(img1_filtered, cmap='gray')
+plt.title('Filtered 1 (Gaussian Blur)')
+plt.axis('off')
+plt.suptitle("Αρχική και Φιλτραρισμένη - Εικόνα 1 (Gaussian noise)")
+plt.show()
+
+#Εικόνα 2
+img2_filtered = median(image2, footprint=disk(1))
+
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.imshow(image2, cmap='gray')
+plt.title('Noisy 2')
+plt.axis('off')
+plt.subplot(1, 2, 2)
+plt.imshow(img2_filtered, cmap='gray')
+plt.title('Filtered 2 (Median)')
+plt.axis('off')
+plt.suptitle("Αρχική και Φιλτραρισμένη - Εικόνα 2 (Salt & Pepper noise)")
+plt.show()
