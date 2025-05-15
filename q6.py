37e98fbdb736b1e7eebd6ae36ca19e8129515930
thomas <thomaskald849@gmail.com>
2025-05-15 20:38:52 +0300

    update

--- q6.py
+++ q6.py
@@ -5,7 +5,7 @@ import cv2
 # Μετατροπή εικόνας σε εικόνα αποχρώσεων γκρί
 image = cv2.imread('/home/thomas/Digital_Image_Processing/DIP-project-1/DIP-project-1/images-project-1/book-cover.jpeg', cv2.IMREAD_GRAYSCALE)
 #text = pytesseract.image_to_string(image)
-#text = pytesseract.image_to_string('/home/thomas/Digital_Image_Processing/DIP-project-1/DIP-project-1/images-project-1/book-cover.jpeg')
+#text = pytesseract.image_to_string('')
 #print(text)
 
 # Gaussian blur για καλύτερα αποτελέσματα
